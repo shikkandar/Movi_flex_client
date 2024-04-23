6ccec22dd0576eca87c35fb6b203bd2813b2854d
shikkandar <mohammedshikkandar1@gmail.com>
2024-04-23 13:36:32 +0530

    404 page update ui

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -6,14 +6,15 @@ import { Link } from 'react-router-dom';
 export const PageNotFound = () => {
   return (
     <div className="bg-white">
-      <Image src={not} fluid className="position-relative" />
-      <div className="text-center">
+      <div className='d-flex w-100 justify-content-center'>
         <Button variant='dark' className='mt-3'>
           <Link className='text-white text-decoration-none' to={'/'}>
             Back to Home
           </Link>
         </Button>
       </div>
+      <Image src={not} fluid />
+      
     </div>
   );
 };
