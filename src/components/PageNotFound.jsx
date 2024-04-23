2d4f8b85a196f0abb78f58b4a700b2445ad2eee9
shikkandar <mohammedshikkandar1@gmail.com>
2024-04-23 13:44:57 +0530

    404 update ui

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
-import { Button, Image } from 'react-bootstrap';
-import not from '../assets/blue.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button, Image } from "react-bootstrap";
+import not from "../assets/blue.png";
+import not1 from "../assets/blue2.png";
+import { Link } from "react-router-dom";
 
 export const PageNotFound = () => {
   return (
     <div className="bg-white">
-      <div className='d-flex w-100 justify-content-center'>
-        <Button variant='dark' className='mt-3'>
-          <Link className='text-white text-decoration-none' to={'/'}>
+      <div className="d-flex w-100 justify-content-center">
+        <Button
+          variant="dark"
+          className="mt-3">
+          <Link
+            className="text-white text-decoration-none"
+            to={"/"}>
             Back to Home
           </Link>
         </Button>
       </div>
-      <Image src={not} fluid />
-      
+      <div className="d-flex w-100 justify-content-center">
+        <Image
+          src={not}
+          fluid
+          className="d-lg-block d-none"
+        />
+        <Image
+          src={not1}
+          fluid
+          className="d-lg-none d-block"
+        />
+      </div>
     </div>
   );
 };
