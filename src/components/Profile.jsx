f28ef1989a24738b92995550db5521ee19bb0553
shikkandar <mohammedshikkandar1@gmail.com>
2024-04-23 20:37:11 +0530

    Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,12 +5,13 @@ import avatar from "../assets/13369169.jpg";
 import convertToBase64 from "../helper/convert";
 import TextField from "@mui/material/TextField";
 import { Container, Form } from "react-bootstrap";
+import { textFieldStyles } from "../cutomCss/inputField";
 
 export const Profile = () => {
   const [edit, setEdit] = useState(false);
-  const [file,setFile]=useState()
+  const [file, setFile] = useState();
   function enableEdit() {
-    setEdit(!edit)
+    setEdit(!edit);
   }
   const onUpload = async (e) => {
     if (e.target.files[0]) {
@@ -26,39 +27,52 @@ export const Profile = () => {
           className="d-flex container img-con2"
           style={{ height: "200px" }}>
           <div className="d-flex flex-column  ">
-            {!edit ?<Image
-              className="img2"
-              src={avatar}
-              fluid
-            />:  <div className="d-flex justify-content-center ">
-            <label htmlFor="profile">
-              <div >
-                <Image 
-                  fluid
-                  src={file || avatar}
-                  className="img2"
-                  alt="avatar"
+            {!edit ? (
+              <Image
+                className="img2"
+                src={avatar}
+                fluid
+              />
+            ) : (
+              <div className="d-flex justify-content-center ">
+                <label htmlFor="profile">
+                  <div >
+                    <Image
+                      fluid
+                      src={file || avatar}
+                      className="img2"
+                      alt="avatar"
+                    />
+                    <div className="upload1">
+                      <h6>Upload</h6>
+                    </div>
+                  </div>
+                </label>
+                <input
+                  onChange={onUpload}
+                  type="file"
+                  id="profile"
+                  name="profile"
+                  style={{ display: "none" }}
                 />
-                <div className="upload1">
-                  <h6>Upload</h6>
-                </div>
               </div>
-            </label>
-            <input
-              onChange={onUpload}
-              type="file"
-              id="profile"
-              name="profile"
-              style={{ display: "none" }}
-            />
-          </div>}
-          
-            <Button
-              className="mt-2"
-              onClick={enableEdit}
-              variant="outline-dark">
-              Edit profile
-            </Button>
+            )}
+
+            {!edit ? (
+              <Button
+                className="mt-2"
+                onClick={enableEdit}
+                variant="outline-dark">
+                Edit profile
+              </Button>
+            ) : (
+              <Button
+                className="mt-2"
+                onClick={enableEdit}
+                variant="outline-dark">
+                Update profile
+              </Button>
+            )}
           </div>
           <div className="mx-3 mt-5">
             <h1 className="text-white">Andy Horwitz</h1>
@@ -66,13 +80,62 @@ export const Profile = () => {
           </div>
         </div>
       </div>
-      <Container >
-        <Form className="d-flex w-100">
-          <TextField
-            id="outlined-basic"
-            label="Outlined"
-            variant="outlined"
-          />
+      <Container>
+        <Form className="profile-form">
+          <div className="d-flex">
+            <TextField
+              id="firstName-outlined-basic"
+              label="First Name"
+              variant="outlined"
+              sx={textFieldStyles}
+              className="m-2 w-50"
+              disabled={!edit}
+            />
+            <TextField
+              id="lastName-outlined-basic"
+              label="Last Name"
+              variant="outlined"
+              sx={textFieldStyles}
+              className="m-2 w-50"
+              disabled={!edit}
+            />
+          </div>
+          <div className="d-flex">
+            <TextField
+              id="email-outlined-basic"
+              label="Email"
+              variant="outlined"
+              sx={textFieldStyles}
+              className="m-2 w-50"
+              disabled={!edit}
+            />
+            <TextField
+              id="phone-outlined-basic"
+              label="Phone"
+              variant="outlined"
+              sx={textFieldStyles}
+              className="m-2 w-50"
+              disabled={!edit}
+            />
+          </div>
+          <div className="d-flex">
+            <TextField
+              id="address-outlined-basic"
+              label="Address"
+              variant="outlined"
+              sx={textFieldStyles}
+              className="m-2 w-50"
+              disabled={!edit}
+            />
+            <TextField
+              id="city-outlined-basic"
+              label="City"
+              variant="outlined"
+              sx={textFieldStyles}
+              className="m-2 w-50"
+              disabled={!edit}
+            />
+          </div>
         </Form>
       </Container>
     </div>
